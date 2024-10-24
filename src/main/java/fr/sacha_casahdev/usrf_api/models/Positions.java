23895c1a9de0e13a04d01ca7a7c148dad528dd66
Sacha <sacha.laurent@iut-dijon.u-bourgogne.fr>
2024-10-24 17:34:12 +0200

    Added Position Enum

---
+++ src/main/java/fr/sacha_casahdev/usrf_api/models/Positions.java
@@ -0,0 +1,15 @@
+package fr.sacha_casahdev.usrf_api.models;
+
+public enum Positions {
+    Goalkeeper,
+    CenterBack,
+    LeftBack,
+    RightBack,
+    DefensiveMidfielder,
+    CentralMidfielder,
+    AttackingMidfielder,
+    LeftWinger,
+    RightWinger,
+    Striker,
+    Substitute,
+}
