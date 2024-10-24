b68f3370ec4b74ee46513b332948becebd7d0adc
Sacha <sacha.laurent@iut-dijon.u-bourgogne.fr>
2024-10-24 17:52:56 +0200

    Added ActionType enum

---
+++ src/main/java/fr/sacha_casahdev/usrf_api/models/ActionType.java
@@ -0,0 +1,19 @@
+package fr.sacha_casahdev.usrf_api.models;
+
+public enum ActionType {
+    CREATE_USER,
+    UPDATE_USER,
+    DELETE_USER,
+    CREATE_TEAM,
+    UPDATE_TEAM,
+    DELETE_TEAM,
+    CREATE_PLAYER,
+    UPDATE_PLAYER,
+    DELETE_PLAYER,
+    CREATE_MATCH,
+    UPDATE_MATCH,
+    DELETE_MATCH,
+    CREATE_PLAYED,
+    UPDATE_PLAYED,
+    DELETE_PLAYED,
+}
