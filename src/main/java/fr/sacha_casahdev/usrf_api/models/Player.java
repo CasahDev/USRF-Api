b40a0b7db989764753999aa9d970991c6af5d654
Sacha <sacha.laurent@iut-dijon.u-bourgogne.fr>
2024-10-24 17:25:02 +0200

    Added Player model

---
+++ src/main/java/fr/sacha_casahdev/usrf_api/models/Player.java
@@ -0,0 +1,39 @@
+package fr.sacha_casahdev.usrf_api.models;
+
+public class Player {
+    private int id;
+    private String first_name;
+    private String last_name;
+
+    public Player(int id, String first_name, String last_name) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+    }
+
+    public Player() {}
+
+    public int getId() {
+        return id;
+    }
+
+    public void setId(int id) {
+        this.id = id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public void setFirst_name(String first_name) {
+        this.first_name = first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public void setLast_name(String last_name) {
+        this.last_name = last_name;
+    }
+}
