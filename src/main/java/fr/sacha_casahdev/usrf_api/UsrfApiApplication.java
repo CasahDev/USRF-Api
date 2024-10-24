dc49b9c70e25604ed0908594128ae2e81f99983e
Sacha <sacha.laurent@iut-dijon.u-bourgogne.fr>
2024-10-24 17:21:54 +0200

    First commit

---
+++ src/main/java/fr/sacha_casahdev/usrf_api/UsrfApiApplication.java
@@ -0,0 +1,13 @@
+package fr.sacha_casahdev.usrf_api;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UsrfApiApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UsrfApiApplication.class, args);
+    }
+
+}
---
+++ src/test/java/fr/sacha_casahdev/usrf_api/UsrfApiApplicationTests.java
@@ -0,0 +1,13 @@
+package fr.sacha_casahdev.usrf_api;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UsrfApiApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
