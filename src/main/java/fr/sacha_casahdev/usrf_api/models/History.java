68fbcb1705501bbe74876722a2984933850bbd80
Sacha <sacha.laurent@iut-dijon.u-bourgogne.fr>
2024-10-24 17:54:17 +0200

    Added History model

---
+++ src/main/java/fr/sacha_casahdev/usrf_api/models/History.java
@@ -0,0 +1,26 @@
+package fr.sacha_casahdev.usrf_api.models;
+
+import lombok.Getter;
+import lombok.Setter;
+
+import java.sql.Time;
+
+@Getter
+@Setter
+public class History {
+    private int id = 0;
+    private User author = null;
+    private Time created_at = null;
+    private ActionType action = null;
+    private String additional_information = null;
+
+    public History() {
+    }
+
+    public History(User author, Time created_at, ActionType action, String additional_information) {
+        this.author = author;
+        this.created_at = created_at;
+        this.action = action;
+        this.additional_information = additional_information;
+    }
+}
