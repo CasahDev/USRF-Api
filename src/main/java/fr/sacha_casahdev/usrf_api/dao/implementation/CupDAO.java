75636c15b96e98b9bafe86bc1e2610d2e7ec4687
Sacha <sacha.laurent@iut-dijon.u-bourgogne.fr>
2024-10-26 21:35:31 +0200

    Added CupDAO interface and implementation, added IJsonable in Cup model

---
+++ src/main/java/fr/sacha_casahdev/usrf_api/dao/implementation/CupDAO.java
@@ -0,0 +1,170 @@
+package fr.sacha_casahdev.usrf_api.dao.implementation;
+
+import fr.sacha_casahdev.usrf_api.dao.DBConnection;
+import fr.sacha_casahdev.usrf_api.dao.interfaces.ICupDAO;
+import fr.sacha_casahdev.usrf_api.models.Cup;
+import fr.sacha_casahdev.usrf_api.models.Scale;
+import org.springframework.http.ResponseEntity;
+
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Map;
+
+public class CupDAO implements ICupDAO {
+    Connection conn;
+
+    public CupDAO() {
+        this.conn = DBConnection.getConnection();
+    }
+
+    @Override
+    public ResponseEntity<Cup> getCupById(int id) {
+        ResponseEntity<Cup> response = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM cups WHERE id = ?");
+            stmt.setInt(1, id);
+
+            ResultSet rs = stmt.executeQuery();
+
+            Cup cup = new Cup();
+            cup.setId(id);
+            cup.setName(rs.getString("name"));
+            cup.setScale(Scale.valueOf(rs.getString("scale")));
+
+            response = ResponseEntity.ok(cup);
+        } catch (Exception e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<List<Cup>> getCups(int page, int limit) {
+        ResponseEntity<List<Cup>> response = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM cups LIMIT ? OFFSET ?");
+            stmt.setInt(1, limit);
+            stmt.setInt(2, (page - 1) * limit);
+
+            ResultSet rs = stmt.executeQuery();
+
+            List<Cup> cups = CupListFromResultSet(rs);
+
+            response = ResponseEntity.ok(cups);
+        } catch (Exception e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
+    }
+
+    private List<Cup> CupListFromResultSet(ResultSet rs) throws Exception {
+        List<Cup> cups = new ArrayList<>();
+
+        while (rs.next()) {
+            Cup cup = new Cup();
+            cup.setId(rs.getInt("id"));
+            cup.setName(rs.getString("name"));
+            cup.setScale(Scale.valueOf(rs.getString("scale")));
+            cups.add(cup);
+        }
+
+        return cups;
+    }
+
+    @Override
+    public ResponseEntity<List<Cup>> getCupsByScale(String scale, int page, int limit) {
+        ResponseEntity<List<Cup>> response = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("SELECT * FROM cups WHERE scale = ? LIMIT ? OFFSET ?");
+            stmt.setString(1, scale);
+            stmt.setInt(2, limit);
+            stmt.setInt(3, (page - 1) * limit);
+
+            ResultSet rs = stmt.executeQuery();
+
+            List<Cup> cups = CupListFromResultSet(rs);
+
+            response = ResponseEntity.ok(cups);
+        } catch (Exception e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<Cup> addCup(Map<String, Object> cup) {
+        ResponseEntity<Cup> response = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("INSERT INTO cups (name, scale) VALUES (?, ?)");
+            stmt.setString(1, (String) cup.get("name"));
+            stmt.setString(2, (String) cup.get("scale"));
+
+            stmt.executeUpdate();
+
+            int id = stmt.getGeneratedKeys().getInt(1);
+
+            Cup c = new Cup();
+            c.setId(id);
+            c.setName((String) cup.get("name"));
+            c.setScale(Scale.valueOf((String) cup.get("scale")));
+
+            response = ResponseEntity.ok(c);
+        } catch (Exception e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<Cup> updateCup(int id, Map<String, Object> cup) {
+        ResponseEntity<Cup> response = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("UPDATE cups SET name = ?, scale = ? WHERE id = ?");
+            stmt.setString(1, (String) cup.get("name"));
+            stmt.setString(2, (String) cup.get("scale"));
+            stmt.setInt(3, (int) cup.get("id"));
+
+            stmt.executeUpdate();
+
+            Cup c = new Cup();
+            c.setId(id);
+            c.setName((String) cup.get("name"));
+            c.setScale(Scale.valueOf((String) cup.get("scale")));
+
+            response = ResponseEntity.ok(c);
+        } catch (Exception e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
+    }
+
+    @Override
+    public ResponseEntity<String> deleteCup(int id) {
+        ResponseEntity<String> response = null;
+
+        try {
+            PreparedStatement stmt = conn.prepareStatement("DELETE FROM cups WHERE id = ?");
+            stmt.setInt(1, id);
+
+            stmt.executeUpdate();
+
+            response = ResponseEntity.ok("Cup deleted");
+        } catch (Exception e) {
+            response = ResponseEntity.internalServerError().build();
+        }
+
+        return response;
+    }
+}
--- src/main/java/fr/sacha_casahdev/usrf_api/models/Cup.java
+++ src/main/java/fr/sacha_casahdev/usrf_api/models/Cup.java
@@ -3,9 +3,11 @@ package fr.sacha_casahdev.usrf_api.models;
 import lombok.Getter;
 import lombok.Setter;
 
+import java.util.Map;
+
 @Getter
 @Setter
-public class Cup {
+public class Cup implements IJsonable{
     int id = 0;
     String name = "";
     String description = "";
@@ -19,4 +21,22 @@ public class Cup {
     }
 
     public Cup() {}
+
+    @Override
+    public String toJson() {
+        return "{" +
+                "\"id\":" + id + "," +
+                "\"name\":\"" + name + "\"," +
+                "\"description\":\"" + description + "\"," +
+                "\"scale\":" + scale.name() +
+                "}";
+    }
+
+    @Override
+    public void fromJson(Map<String, Object> json) {
+        if (json.containsKey("id")) id = (int) json.get("id");
+        if (json.containsKey("name")) name = (String) json.get("name");
+        if (json.containsKey("description")) description = (String) json.get("description");
+        if (json.containsKey("scale")) scale = Scale.valueOf((String) json.get("scale"));
+    }
 }
